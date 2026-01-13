ad9694571ee9d3c11750ba65124dda0fe29a4120
soufiane redouane <soulf83@gmail.com>
2026-01-13 17:23:29 +0100

    Pattern reversed

---
+++ src/main/java/org/exercice/PatternRev.java
@@ -0,0 +1,20 @@
+package org.exercice;
+// Print this pattern:
+//*****
+//****
+//***
+//**
+//*
+
+public class PatternRev {
+    public static void main (String[] args) {
+        for ( int i = 4 ; i >= 0 ; i--){
+
+            for ( int j = 0 ; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
